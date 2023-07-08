12ee544a7fe133ff78822685bec760e276d3b225
a-sprasad1 <shashwat.prasad.tesltra@gmail.com>
2023-07-09 07:32:38 +1000

    VM creation

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "subscription_id" {
   type        = string
   description = "Enter here the subscription id"
-  default     = "80ea84e8-afce-4851-928a-9e2219724c69"
+  default     = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
 }
 variable "tenant_id" {
   type        = string
@@ -11,46 +11,20 @@ variable "tenant_id" {
 variable "client_secret" {
   type        = string
   description = "Enter here the subscription id"
-  default     = "uDp8Q~OA72nxmuovzJ9ZK7Id-BuT_KqRvNRx6auT"
+  default     = "2XZ8Q~-wLmX5EnnsjVqjnluybICFY_gl6EkfNcFn"
 }
 variable "client_id" {
   type        = string
   description = "Enter here the Client id"
-  default     = "be2d1fca-b7b7-4b98-b0d5-a77ddbf8828d"
+  default     = "406165d9-3c89-4525-a905-b2d3c70a4f9c"
 }
 variable "resource_group_name" {
   type        = string
   description = "Enter here the Resource Group"
-  default     = "1-7996a50d-playground-sandbox"
+  default     = "1-8ac1f286-playground-sandbox"
 }
 variable "location" {
   type        = string
   description = "Enter here the location"
   default     = "East US"
-}
-
-variable "virtual_network" {
-  type        = string
-  description = "Enter here the location"
-  default     = "DB_VNET"
-}
-variable "virtual_machine" {
-  type        = string
-  description = "Enter here the location"
-  default     = "DB1"
-}
-variable "network_interface" {
-  type        = string
-  description = "Enter here the location"
-  default     = "DB1_NIC"
-}
-variable "Subnet" {
-  type        = string
-  description = "Enter here the location"
-  default     = "DB_SUBNET"
-}
-variable "Public_IP" {
-  type        = string
-  description = "DB1_PUB"
-  default     = "a394e41c-cf8d-458e-ac1b-ddae1aa15629"
 }
--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,76 +1,3 @@
-/*resource "azurerm_virtual_network" "DB_Vnet" {
-  name                = var.virtual_network
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_subnet" "DB_sub" {
-  name                 = var.Subnet
-  resource_group_name  = "1-955a11d0-playground-sandbox"
-  virtual_network_name = "DB_VNET"
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_public_ip" "DB_pub_IP" {
-  name                = var.Public_IP
-  location            = var.location
-  resource_group_name = "1-955a11d0-playground-sandbox"
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "DB-nic" {
-  name                = var.network_interface
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name                          = var.network_interface
-    subnet_id                     = azurerm_subnet.DB_sub.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.DB_pub_IP.id
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "DB_VM" {
-  name                = var.virtual_machine
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  size                = "Standard_DS1_v2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.DB-nic.id,
-  ]
-  admin_password                  = "Proximus#18"
-  disable_password_authentication = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}*/
-
-/*provider "azurerm" {
-
-  client_id       = "60e570f9-56f6-4052-ab18-36dafd651556"
-  client_secret   = "rTA8Q~N2moCbHk6XWr8xoWLc1OBXj4ESi~a5CbJD"
-  tenant_id       = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
-  subscription_id = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
-  features {}
-  skip_provider_registration = true
-}
-
-/*resource "azurerm_resource_group" "DB_RG" {
-  name     = var.resource_group_name
-  location = var.location
-}*/
-
 resource "azurerm_virtual_network" "DB_Vnet" {
   name                = "DB_Vnet"
   address_space       = ["10.0.0.0/16"]
@@ -91,18 +18,7 @@ resource "azurerm_public_ip" "DB_pub_IP_1" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 }
-/*resource "azurerm_public_ip" "DB_pub_IP_2" {
-  name                = "DB-public-ip-2"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-}
-resource "azurerm_public_ip" "DB_pub_IP_3" {
-  name                = "DB-public-ip-3"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-}*/
+
 resource "azurerm_public_ip" "MON_pub_IP_4" {
   name                = "MON-public-ip-4"
   location            = var.location
@@ -122,28 +38,6 @@ resource "azurerm_network_interface" "DB-nic-1" {
   }
 }
 
-/*resource "azurerm_network_interface" "DB-nic-2" {
-  name                = "DB-nic_2"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name                          = "DB-nic-configuration"
-    subnet_id                     = azurerm_subnet.DB_sub.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.DB_pub_IP_2.id
-  }
-}
-resource "azurerm_network_interface" "DB-nic-3" {
-  name                = "DB-nic-3"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name                          = "DB-nic-configuration"
-    subnet_id                     = azurerm_subnet.DB_sub.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.DB_pub_IP_3.id
-  }
-}*/
 resource "azurerm_network_interface" "MON-nic" {
   name                = "MON-nic"
   location            = var.location
@@ -179,54 +73,7 @@ resource "azurerm_linux_virtual_machine" "DB_VM_1" {
     version   = "latest"
   }
 }
-/*resource "azurerm_linux_virtual_machine" "DB_VM_2" {
-  name                = "DBVM2"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  size                = "Standard_D2s_v3"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.DB-nic-2.id,
-  ]
-  admin_password                  = "Proximus#18"
-  disable_password_authentication = false
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}
-resource "azurerm_linux_virtual_machine" "DB_VM_3" {
-  name                = "DBVM3"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  size                = "Standard_D2s_v3"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.DB-nic-3.id,
-  ]
-  admin_password                  = "Proximus#18"
-  disable_password_authentication = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}*/
 resource "azurerm_linux_virtual_machine" "MON1" {
   name                = "MON1"
   location            = var.location
